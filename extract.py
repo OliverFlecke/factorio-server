ff56c4bacec5ed2c250960c065007c7ac7fe2a09
Oliver Fleckenstein <oliverfl@live.dk>
2023-03-22 22:35:10 +0000

    ci: Setting the version correctly in the environment

--- extract.py
+++ extract.py
@@ -10,8 +10,5 @@ response = requests.get(url)
 pattern = '/download/archive/(?P<version>\d+\.\d+\.\d+)'
 versions = re.findall(pattern, response.text)
 
-print(versions)
 stable = versions[0]
-print(f'Stable: {stable}')
-
-os.environ["GITHUB_ENV"] = os.environ["GITHUB_ENV"] + '\n' + f'VERSION={stable}'
+print(f'{stable}')
